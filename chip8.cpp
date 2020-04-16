72e32f1849b2c18a37d69ad9ed091627a57f8547
Jonathan Lee <jonathanlee457@gmail.com>
2020-04-16 18:35:09 -0400

    implemented some instructions

--- chip8.cpp
+++ chip8.cpp
@@ -1,16 +1,27 @@
+#include <fstream>
+#include <stdlib.h>
+#include <time.h>
 #include "chip8.hpp"
+
+// Initialize cpu
 chip8::chip8(const screen &s) :
 	opcode {0},
-	memory(4096), 
-	v(16), 
+	memory(MEMORY_SIZE, 0), 
+	v(NUM_REGISTERS, 0), 
 	i {0},	
-	pc {0x200},
+	pc {START_PC},
 	sp {0}, 
-	stack(16)
+	stack(STACK_SIZE, 0),
+	dt {0},
+	st {0},
 {
-
+	// Load in fontset into memory
+	for (unsigned int i = 0; i < NUM_FONTS; i++) {
+		memory[FONTSET_START + i] = fontset[i];
+	}
 }
 
+// Execute one cycle of cpu
 void chip8::executeCycle()
 {
 	opcode = (memory[pc] << 8) | memory[pc + 1];
@@ -24,5 +35,60 @@ void chip8::executeCycle()
 			// Do something with sound
 		}
 		st--;
+	}
+}
+
+// Read a ROM file into memory
+void chip8::readRom(const std::string &filename) 
+{
+	std::ifstream ifs (filename, std::ios::binary);
+
+	if (ifs) {
+		// Copy file into a buffer, then write buffer to memory
+		std::vector<unsigned char> buff(std::istreambuf_iterator<char>(ifs), {});
+		for (unsigned int i = 0; i < buff.size(); i++) {
+			memory[START_PC + i] = buff[i];
+		}
+	}
+}
+
+// The following instructions are implemented based off technical reference: 
+// http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#1nnn
+
+// Clears the screen
+void chip8::cls()
+{
+	// TODO
+}
+
+// Return from subroutine
+void chip8::ret()
+{
+	pc = stack[sp];
+	sp--;
+}
+
+// Jump to location 
+void chip8::jp()
+{
+	pc = opcode & 0x0FFFu;
+}
+
+// Call subroutine
+void chip8::call()
+{
+	sp++;
+	stack[sp] = pc;
+	pc = opcode & 0x0FFFu;
+}
+
+// Conditional skip next instruction
+void chip8::se()
+{
+	unsigned short x = (opcode & 0x0F00u) >> 8u;
+	unsigned short byte = opcode & 0x00FFu;
+
+	if (v[x] == byte) {
+		pc += 2;
 	}
 }
--- chip8.hpp
+++ chip8.hpp
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <random>
+#include <string>
 #include <vector>
 #include "screen.hpp"
 
@@ -7,6 +10,35 @@
 class chip8
 {
 private:
+	// Constants
+	const unsigned short START_PC = 0x200;
+	const unsigned int MEMORY_SIZE = 4096;
+	const unsigned int NUM_REGISTERS = 16;
+	const unsigned int STACK_SIZE = 16;
+	const unsigned int NUM_FONTS = 80;
+	const unsigned int FONTSET_START = 0x50;
+
+	// Fontset
+	unsigned char fontset[80] =
+	{
+	    0xF0, 0x90, 0x90, 0x90, 0xF0, //0
+	    0x20, 0x60, 0x20, 0x20, 0x70, //1
+	    0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
+	    0xF0, 0x10, 0xF0, 0x10, 0xF0, //3
+	    0x90, 0x90, 0xF0, 0x10, 0x10, //4
+	    0xF0, 0x80, 0xF0, 0x10, 0xF0, //5
+	    0xF0, 0x80, 0xF0, 0x90, 0xF0, //6
+	    0xF0, 0x10, 0x20, 0x40, 0x40, //7
+	    0xF0, 0x90, 0xF0, 0x90, 0xF0, //8
+	    0xF0, 0x90, 0xF0, 0x10, 0xF0, //9
+	    0xF0, 0x90, 0xF0, 0x90, 0x90, //A
+	    0xE0, 0x90, 0xE0, 0x90, 0xE0, //B
+	    0xF0, 0x80, 0x80, 0x80, 0xF0, //C
+	    0xE0, 0x90, 0x90, 0x90, 0xE0, //D
+	    0xF0, 0x80, 0xF0, 0x80, 0xF0, //E
+	    0xF0, 0x80, 0xF0, 0x80, 0x80  //F
+	};
+ 
 	// Current 2-byte opcode
 	unsigned short opcode;
 
@@ -24,10 +56,31 @@ private:
 	unsigned short sp;
 	std::vector<unsigned short> stack;
 
-	// Delay and sound 60 Hz timer registers
+	// Delay and sound timer registers
 	unsigned char dt;
 	unsigned char st;
 public:
+	// Initialize cpu
 	chip8(const screen&);
+
+	// Execute one cycle of cpu
 	void executeCycle();
+
+	// Read a ROM file into memory
+	void readRom(const std::string&);
+
+	// Clears the screen
+	void cls();
+
+	// Return from subroutine
+	void ret();
+
+	// Jump to location
+	void jp();
+
+	// Call subroutine
+	void call();
+
+	// Conditional skip next instruction
+	void se();
 };
